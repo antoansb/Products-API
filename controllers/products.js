e04881b51ded2f62a83ea6b9349e30891d0748ed
Lazar Antonijevic <lazar.antonijevic@appollo41.com>
2023-06-15 02:02:43 +0200

    Basic filtering by name, featured & company

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,21 @@
 const Product = require('../models/product');
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find();
+  const { name, featured, company } = req.query;
+  const queryObject = {};
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+
+  const products = await Product.find(queryObject);
   res.status(200).json({ products });
 };
 
